0d57665d240b4441aad1fb4766b74a9a5303ce52
Chad Adams <nshusa99@gmail.com>
2019-10-29 16:41:11 -0500

    Don't show explicit music

--- server/service/music.service.js
+++ server/service/music.service.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const constants = require('../util/constants')
 
 function find(query) {
-    return axios.get(`${constants.ITUNES_BASE_API}/search?term=${query.search}&media=music`)
+    return axios.get(`${constants.ITUNES_BASE_API}/search?term=${query.search}&media=music&explicit=No`)
 }
 
 exports.find = find
